0a039c3fbb9df20b29cf075527ee00e14dcbf7d9
marcory-hub <marcorydummyvandijk@gmail.com>
2024-06-16 18:28:40 +0200

    Make webcam image square 224x224px

--- camTest.py
+++ camTest.py
@@ -10,15 +10,28 @@ model = load_model('/Users/md/Developer/TMPOLEN23E/keras_model.h5')
 
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
-class_labels=['Anadenanthera','Arecaceae','Arrabidaea','Cecropia','Chromolaena','Combretum','Croton','Dipteryx','Eucalipto','Farmea','Hyptis','Mabea','Matayba','Mimosa','Myrcia','Protium','Qualea','Schinus','Senegalia','Serjania','Syagrus','Tridax','Urochloa']
+
+class_labels = ['Anadenanthera', 'Arecaceae', 'Arrabidaea', 'Cecropia', 'Chromolaena', 'Combretum', 'Croton', 'Dipteryx', 'Eucalipto', 'Farmea', 'Hyptis', 'Mabea', 'Matayba', 'Mimosa', 'Myrcia', 'Protium', 'Qualea', 'Schinus', 'Senegalia', 'Serjania', 'Syagrus', 'Tridax', 'Urochloa']
 
 while True:
     # Capture a frame from the webcam
     ret, frame = cap.read()
-    print(frame.shape)
     
-    # Pre-process the frame for classification
-    img = cv2.resize(frame, (224, 224))
+    # Get frame dimensions
+    frame_height, frame_width, _ = frame.shape
+
+    # Get the minimum dimension as the crop size for the square
+    crop_size = min(frame_height, frame_width)
+
+    # Define top left corner for the square crop (ensuring we stay within frame boundaries)
+    top = 0 if frame_height == crop_size else int((frame_height - crop_size) / 2)
+    left = 0 if frame_width == crop_size else int((frame_width - crop_size) / 2)
+
+    # Crop the frame to a square
+    cropped_frame = frame[top:top+crop_size, left:left+crop_size]
+    
+    # Pre-process the cropped frame by resizing to 224x224
+    img = cv2.resize(cropped_frame, (224, 224))
     img_tensor = image.img_to_array(img)
     img_tensor = np.expand_dims(img_tensor, axis=0)
     img_tensor /= 255.
@@ -28,13 +41,11 @@ while True:
     
     # Get the class label for the frame
     class_index = np.argmax(prediction[0])
-    
-    
-    # Get the class label for the frame
-    class_label =class_labels[class_index]
+    class_label = class_labels[class_index]
     print(class_label)
     
-    # Display the frame with the class label
+    # Display the original frame with a bounding box around the cropped region
+    cv2.rectangle(frame, (left, top), (left+crop_size, top+crop_size), (0, 255, 0), 2)
     cv2.putText(frame, class_label, (10, 30), cv2.FONT_HERSHEY_COMPLEX,
                 1.0, (0, 0, 255), 3)
     cv2.imshow('Webcam', frame)
